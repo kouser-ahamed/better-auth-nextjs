a41961a1fb6ed323bd2c05caa48fa113f1ab2960
kouser-ahamed <ahamed15-5643@diu.edu.bd>
2026-04-23 14:28:34 +0600

    feat: comment out email verification requirement in auth configuration

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -8,6 +8,7 @@ const db = client.db('better-auth-db');
 export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
+    // requireEmailVerification: true,
   },
 
   database: mongodbAdapter(db, {
